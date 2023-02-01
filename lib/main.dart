b4d5c3aa735a0adf91d92d46ea4a778e3128b95f
danaFlorescu <mariadana977@gmail.com>
2023-02-01 16:03:07 +0200

    Added the male card and created the reusable card widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() => runApp(BMICalculator());
 
@@ -8,6 +9,12 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        appBarTheme: AppBarTheme(
+          color: Color(0xFF0E101C),
+        ),
+        scaffoldBackgroundColor: Color(0xFF0E101C),
+      ),
       home: InputPage(),
     );
   }
@@ -27,8 +34,51 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: ReusableCard(),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Column(
+        children: [
+          FaIcon(
+            FontAwesomeIcons.mars,
+            color: Colors.black,
+            size: 50.0,
+          ),
+          Text(
+            'MALE',
+            style: TextStyle(
+              fontSize: 18.0,
+              color: Colors.black54,
+            ),
+          ),
+        ],
       ),
     );
   }
